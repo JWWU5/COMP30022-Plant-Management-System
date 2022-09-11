eda73c37fad3c7a25791a672df9f57ccea753b87
xuanhzhang <xuanhzhang@student.unimelb.edu.au>
2022-09-11 22:53:15 +1000

    regiser and login set up

--- pvsz-api/src/controllers/plantController.js
+++ pvsz-api/src/controllers/plantController.js
@@ -1,13 +1,13 @@
-const { Plant } = require("./../models");
+const { Plant } = require('./../models');
 
 exports.register = async (req, res, next) => {
-    console.log(req.body, "data");
-    const plantModel = new Plant(req.body);
-    const result = await plantModel.save();
-    res.status(201).json(result);
+  console.log(req.body, 'data');
+  const plantModel = new Plant(req.body);
+  const result = await plantModel.save();
+  res.status(201).json(result);
 };
 
 exports.list = async (req, res, next) => {
-    const result = await Plant.find();
-    res.status(200).json(result);
+  const result = await Plant.find();
+  res.status(200).json(result);
 };
--- pvsz-api/src/controllers/userController.js
+++ pvsz-api/src/controllers/userController.js
@@ -1,12 +1,88 @@
 const { User } = require("./../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
     console.log(req.body, "data");
-    const userModel = new User(req.body);
-    const result = await userModel.save();
-    res.status(201).json(result);
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        console.log(hashedPassword, "after hashing");
+        const user = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            birthdayDate: req.body.birthdayDate,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+        const result = await user.save();
+        console.log(result, "register result");
+        res.status(201).send({
+            message: "User Created Successfully",
+            result,
+        });
+    } catch (e) {
+        console.log(e, "fail");
+        res.status(200).json({
+            code: 1,
+            message: "register fail",
+        });
+    }
 };
 
+exports.login = async (req, res, next) => {
+    console.log(req.body, "data");
+    User.findOne({ userName: req.body.userName })
+
+        // if email exists
+        .then((user) => {
+            // compare the password entered and the hashed password found
+            bcrypt
+                .compare(req.body.password, user.password)
+
+                // if the passwords match
+                .then((passwordCheck) => {
+                    // check if password matches
+                    if (!passwordCheck) {
+                        return response.status(400).send({
+                            message: "Passwords does not match",
+                            error,
+                        });
+                    }
+
+                    //   create JWT token
+                    const token = jwt.sign(
+                        {
+                            userId: user._id,
+                            userName: user.userName,
+                        },
+                        "RANDOM-TOKEN",
+                        { expiresIn: "24h" }
+                    );
+
+                    //   return success response
+                    res.status(200).send({
+                        message: "Login Successful",
+                        userName: user.userName,
+                        token,
+                    });
+                })
+                // catch error if password does not match
+                .catch((error) => {
+                    res.status(400).send({
+                        message: "Passwords does not match",
+                        error,
+                    });
+                });
+        })
+        // catch error if email does not exist
+        .catch((e) => {
+            response.status(404).send({
+                message: "Email not found",
+                e,
+            });
+        });
+};
 exports.list = async (req, res, next) => {
     const result = await User.find();
     res.status(200).json(result);
