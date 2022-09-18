a638e9a36e5ff0036d4c6def17f9b8169ac21d6b
WUGE <wuge@student.unimelb.edu.au>
2022-09-19 01:15:19 +1000

    Import the font family in all css files, but need further testing to check if it works correctly. Finish the profile page but need further testing.

--- pvsz-web/src/App.js
+++ pvsz-web/src/App.js
@@ -9,7 +9,8 @@ import ContactUS from "./components/ContactUS.component";
 import SignIn from "./components/SignIn.component";
 import SignUp from "./components/SignUp.component";
 import AppLayout from "./components/AppLayout";
-import DynamicButton from "./components/dynamicButton";
+// import DynamicButton from "./components/dynamicButton";
+import Profile from "./components/Profile.component";
 import "boxicons/css/boxicons.min.css";
 import "./App.css";
 
@@ -29,7 +30,8 @@ function App() {
                 <Route path="/contact-us" element={<ContactUS />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/dynamic-button" element={<DynamicButton />} />
+                {/* <Route path="/dynamic-button" element={<DynamicButton />} /> */}
+                <Route path="profile" element={<Profile />} />
             </Routes>
         </Router>
     );
