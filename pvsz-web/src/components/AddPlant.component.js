a818c19777217645a2d89ae7116d12d6a1307b0d
WUGE <wuge@student.unimelb.edu.au>
2022-09-19 18:22:25 +1000

    Begin to write the add plant page.

---
+++ pvsz-web/src/components/AddPlant.component.js
@@ -0,0 +1,10 @@
+import { Grid } from '@mui/material';
+import Header from './GreenHeader';
+import avatar from "../assets/images/avatar.png";
+import { useState } from "react";
+import "./Profile.css";
+import './dynamicButton.scss';
+
+export default function AddPlant() {
+
+}
