4c6a6dfa98e310112840cedcffea6384c1656685
WUGE <wuge@student.unimelb.edu.au>
2022-09-21 12:13:21 +1000

    Fix a bug in sidebar and update the responsive design of the addPlant page.

--- pvsz-web/src/components/AddPlant.component.js
+++ pvsz-web/src/components/AddPlant.component.js
@@ -7,12 +7,33 @@ import './dynamicButton.scss';
 
 export default function AddPlant() {
     const [plantName, setPlantName] =useState("");
-    const [waterRate, setWaterRate] = useState(" days");
+    const [waterRate, setWaterRate] = useState("");
     const [lastWaterTime, setLastWaterTime] = useState("");
-    const [sunshineRate, setSunshineRate] = useState(" days");
+    const [sunshineRate, setSunshineRate] = useState("");
     const [lastSunshineTime, setLastSunshineTime] = useState("");
     const [belongGroup, setBelongGroup] = useState("");
     const [otherDetail, setOtherDetail] = useState("");
+    const [invalidInput, setInvalidInput] = useState(false);
+
+    // function checkNumInput(inputvalue) {
+    //     var pattern = /[0-9]/;
+    //     var text = inputvalue.replace(" days", '');
+    //     console.log(text);
+    //     if (pattern.test(text) === false) {
+    //         setInvalidInput(true);
+    //     }
+    //     else {
+    //         setInvalidInput(false);
+    //     }
+    // };
+
+    // const updateWaterRate = (e) => {
+    //     checkNumInput(e.target.value);
+    //     if (!invalidInput) {
+    //         setWaterRate(e.target.value);
+    //     }
+    //     // console.log(waterRate);
+    // };
 
     return (
         <body>
@@ -37,13 +58,15 @@ export default function AddPlant() {
                     ></input> 
                 </div>
                 <div className='plantValueDiv'>
-                    <h3 className='plantValueTitle'>Watering in</h3>
+                    <h3 className='plantValueTitle'>Watering in (days)</h3>
                     <input
                         className='plantValueBlock'
-                        type="text"
+                        type="number"
                         value={waterRate}
                         onChange={(e) => setWaterRate(e.target.value)}
                     ></input> 
+                    <p></p>
+                    { invalidInput && <span className="errorMessage">Sorry, we only accept interger here. </span> }
                 </div>
                 <div className='plantValueDiv'>
                     <h3 className='plantValueTitle'>Last Watering Time</h3>
@@ -55,9 +78,9 @@ export default function AddPlant() {
                     ></input> 
                 </div>
                 <div className='plantValueDiv'>
-                    <h3 className='plantValueTitle'>Sublight in</h3>
+                    <h3 className='plantValueTitle'>Sublight in (days)</h3>
                     <input
-                        type="text"
+                        type="number"
                         className='plantValueBlock'
                         value={sunshineRate}
                         onChange={(e) => setSunshineRate(e.target.value)}
@@ -72,10 +95,16 @@ export default function AddPlant() {
                         onChange={(e) => setLastSunshineTime(e.target.value)}
                     ></input> 
                 </div>
+                {/* <div className='plantValueDiv'>
+                    <label>Choose a group:</label>
+                    <select className='palntValueBlock'>
+
+                    </select>
+                </div> */}
                 <div className='plantValueDiv'>
                     <h3 className='plantValueTitle'>Other details</h3>
                     <input
-                        className='plantValueBlock'
+                        className='plantValueDetail'
                         type="text"
                         value={otherDetail}
                         onChange={(e) => setOtherDetail(e.target.value)}
