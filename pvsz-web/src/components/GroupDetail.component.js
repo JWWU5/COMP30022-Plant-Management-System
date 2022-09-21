078a42cd4efb9bf8418d20cc451d608aff4a1740
JWWU5 <51792182+JWWU5@users.noreply.github.com>
2022-09-22 02:25:28 +0800

    add liked button

--- pvsz-web/src/components/GroupDetail.component.js
+++ pvsz-web/src/components/GroupDetail.component.js
@@ -21,10 +21,8 @@ import { useNavigate } from 'react-router-dom';
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
 
-
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export default function GroupDetail() {
 
@@ -48,15 +46,9 @@ export default function GroupDetail() {
             <main>
                 <div class="group_name">
                     <h5>LivingRoom</h5>
+                    <Checkbox {...label} color='error' sx={{height: 60}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
                 </div>
                 <div class="bg">
-                    
-                        <div class="icons">
-                            <AddCircleOutlineIcon sx={{color: '#ffffff', width: 30}} onClick={handleAddIcon}/>
-                            <RemoveCircleOutlineIcon sx={{color: '#ffffff', width: 30}} onClick={handleDeleteIcon}/>
-                        </div>
-                   
-                    
                     <div class="plant">
                         <Stack spacing={3} justify-Content="center">
                             <div class="search">
@@ -79,9 +71,8 @@ export default function GroupDetail() {
                                     <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
                                         <SearchIcon />
                                     </IconButton>
-
-
                                 </Paper>
+                                
                             </div>
                             <Divider />
                             <Box display='flex' justify-Content="center" sx={{
@@ -91,9 +82,9 @@ export default function GroupDetail() {
                                 alignItems: 'center',
                                 borderRadius: 25}}>
                                 <Avatar src="avatar.jpg" sx={{ml: 2.5}}/>
-                                <a>Group_1</a>
+                                <a>Plant_1</a>
                                 <Grid container justifyContent="flex-end">
-                                    <ArrowForwardIosOutlinedIcon onClick={toGroupDetail} sx={{mr: 2.5}} />
+                                    <ArrowForwardIosOutlinedIcon sx={{mr: 2.5}} />
                                 </Grid>
                             </Box>
                             <Box display='flex' justify-Content="center" sx={{
@@ -103,9 +94,9 @@ export default function GroupDetail() {
                                 alignItems: 'center',
                                 borderRadius: 25}}>
                                 <Avatar src="avatar1.jpg" sx={{ml: 2.5}}/>
-                                <a>Group_2</a>
+                                <a>Plant_2</a>
                                 <Grid container justifyContent="flex-end">
-                                    <ArrowForwardIosOutlinedIcon onClick={toGroupDetail} sx={{mr: 2.5}} />
+                                    <ArrowForwardIosOutlinedIcon sx={{mr: 2.5}} />
                                 </Grid>
                             </Box>
                         </Stack>
