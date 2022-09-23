37368b419a29ad990b4c36e1e2361aa8170d1bb9
WUGE <wuge@student.unimelb.edu.au>
2022-09-23 18:43:55 +1000

    Finish Setting page and the responsive design works good for 6 phone sizes. Still need more testing.

---
+++ pvsz-web/src/components/Setting.component.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Setting.css";
+import "./Ranking.css";
+import Header from "./Header";
+import { useNavigate } from "react-router-dom";
+
+import Grid from '@mui/material/Grid';
+import Switch from '@mui/material/Switch';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+export default function Setting() {
+    let navigate = useNavigate();
+
+    function handleChangePassword() {
+        navigate("/change-password");
+    }
+
+    function handlePrivatePolicy() {
+        navigate("/private-policy");
+    }
+
+    function handleLogout() {
+        navigate("/");
+    }
+
+    return (
+        <body>
+            <Header />
+            <div className="rankingContentDiv">
+                <div className="rankingContentRec">
+                    <h1 className="rankingTitle">SETTING</h1>
+                    <Grid container direction="column">
+                        <div className="settingButtonDiv">
+                            <Grid container spacing={0}>
+                                <Grid item xs={6}>
+                                    <p>Darkmode</p>   
+                                </Grid>
+                                <Grid item xs={6} alignContent="right">
+                                    <div className="switchButton">
+                                        <Switch defaultChecked size="small" />
+                                    </div>
+                                </Grid>   
+                            </Grid>
+                        </div>
+                        <div className="settingButtonDiv">
+                            <Grid container spacing={0}>
+                                <Grid item xs={8}>
+                                    <p>Change Password</p>   
+                                </Grid>
+                                <Grid item xs={4} alignContent="right">
+                                    <div className="rightArrowIcon">
+                                        <ChevronRightIcon onClick={handleChangePassword} />
+                                    </div>
+                                </Grid>   
+                            </Grid>
+                        </div>
+                        <div className="settingButtonDiv">
+                            <Grid container spacing={0}>
+                                <Grid item xs={8}>
+                                    <p>Provite Policy</p>   
+                                </Grid>
+                                <Grid item xs={4} alignContent="right">
+                                    <div className="rightArrowIcon">
+                                        <ChevronRightIcon onClick={handlePrivatePolicy} />
+                                    </div>
+                                </Grid>   
+                            </Grid>
+                        </div>
+                    </Grid>
+                </div>
+            </div>
+            <Grid 
+                container 
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <div className="logoutButton" onClick={handleLogout}>
+                    <p>LOG OUT</p>
+                </div>
+            </Grid>
+        </body>
+    )
+}
