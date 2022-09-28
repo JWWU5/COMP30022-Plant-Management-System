eb185641c02fa6613927e02037c5f2fb8a6db887
Tykkk1026 <790080910@qq.com>
2022-09-28 20:41:23 +1000

    try deploy in dev

--- pvsz-api/src/index.js
+++ pvsz-api/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require("path")
 const app = express();
 app.use(cors());
 // Accept json data
+require("dotenv").config();
 app.use(express.json({
     extended: true,
     limit: '50mb'
@@ -16,7 +17,12 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1', require('./router'));
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "pvsz-web", "build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "pvsz-web", "build", "index.html"));
+    });
+}
 app.listen(process.env.PORT || 5000, () => {
     console.log('the server is running:localhost:5000');
 });
