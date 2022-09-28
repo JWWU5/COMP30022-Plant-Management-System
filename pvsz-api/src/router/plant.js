d17c02091188cbe457e6ac1f36d2f6dbd53bf60b
Zhenchen WAN <zhenchen.wan@student.unimelb.edu.au>
2022-09-28 21:42:56 +1000

    main update

--- pvsz-api/src/router/plant.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const plantController = require("./../controllers/plantController");
-
-router.post("/add", plantController.register).get("/", plantController.list);
-
-module.exports = router;
